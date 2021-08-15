b73051fe1712396494fb2c5f44a340d5ddd9403d
Linhpvt <linhpvt60@gmail.com>
2021-08-15 17:41:31 +0700

    update webpack webpack-cli html-webpack-plugin latest version

--- microfrontend/helloworld/src/index.js
+++ microfrontend/helloworld/src/index.js
@@ -1,5 +1,5 @@
-import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
-import Heading from './components/heading/heading.js';
+import HelloWorldButton from './components/hello-world-button/hello-world-button';
+import Heading from './components/heading/heading';
 
 const heading = new Heading();
 heading.render();
@@ -7,7 +7,7 @@ const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
 
 if (process.env.NODE_ENV === 'production') {
-    console.log('Production mode');
+  console.log('Production mode');
 } else if (process.env.NODE_ENV === 'development') {
-    console.log('Development mode');
+  console.log('Development mode');
 }
